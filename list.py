aa11a75a68e3c88e5123e2f75dc888f2f3234d92
imgumx <imgumx@gmail.com>
2021-10-04 02:27:03 +0900

    환경설정 완료

--- list.py
+++ list.py
@@ -1,10 +1,17 @@
 import win32com.client
 
-instCpCodeMgr = win32com.client.Dispatch("CpUtil.CpCodeMgr")
-codeList = instCpCodeMgr.GetStockListByMarket(1)
+# 연결 여부 체크
+objCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
+bConnect = objCpCybos.IsConnect
+if (bConnect == 0):
+    print("PLUS가 정상적으로 연결되지 않음. ")
+    exit()
 
-for i, code in enumerate(codeList):
-    secondCode = instCpCodeMgr.GetStockSectionKind(code)
-    name = instCpCodeMgr.CodeToName(code)
-    print(i, code, secondCode, name)
+# instCpCodeMgr = win32com.client.Dispatch("CpUtil.CpCodeMgr")
+# codeList = instCpCodeMgr.GetStockListByMarket(1)
+#
+# for i, code in enumerate(codeList):
+#     secondCode = instCpCodeMgr.GetStockSectionKind(code)
+#     name = instCpCodeMgr.CodeToName(code)
+#     print(i, code, secondCode, name)
 
