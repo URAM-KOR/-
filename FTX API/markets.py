1491f7da3f286cc9798c4c3ff9b86abdd3aa8bf0
imgumx <imgumx@gmail.com>
2021-10-06 19:35:16 +0900

    종목 감시기능 적용

--- FTX API/markets.py
+++ FTX API/markets.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import requests
+import datetime
+import time
+
+# yesterday = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
+today = datetime.datetime.today()
+today = time.mktime(today.timetuple())
+
+Previous_Date = datetime.datetime.today() - datetime.timedelta(days=1)
+Previous_Date = time.mktime(Previous_Date.timetuple())
+#Second as a decimal number [00,61] (or Unix Timestamp)
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
@@ -11,7 +21,24 @@ print(markets)
 
 # 선물시장 필터링
 futures = markets.loc[markets['name'].str.contains('PERP',case=False)]
-print(futures.sort_values('change24h',ascending=False).head(3))
-targetList = futures.sort_values('change24h',ascending=False).head(3)['name']
+print(futures.sort_values('change1h',ascending=False).head(3))
+
+# targetList=[]
+# for i in futures['name']:
+#     recent = requests.get(f'https://ftx.com/api/markets/{i}/candles?resolution=15&start_time={Previous_Date}').json()
+#     recent = pd.DataFrame(recent['result'])
+#     recent = recent.loc[:, ['open', 'close', 'high', 'low']].iloc[-1]['open']
+#     change_day = requests.get(
+#                 f'https://ftx.com/api/markets/{i}/candles?resolution=86400&start_time={Previous_Date}').json()
+#     change_day = pd.DataFrame(change_day['result'])
+#     change_day = change_day.loc[:, ['open', 'close', 'high', 'low']]
+#     change_day['name'] = i
+#     change_day['change_day'] = (change_day['open']-recent)/change_day['open']
+#     targetList.append(change_day)
+#
+# targetList = pd.dataFrame(targetList)
+# print('tttttttttttttttt',targetList)
+
+targetList = futures.sort_values('change1h',ascending=False)['name'].head(3)
 for i in targetList:
     print(i)
--- FTX API/order.py
+++ FTX API/order.py
@@ -6,7 +6,7 @@ from time import sleep
 import datetime
 import ccxt
 import ftx
-import markets
+
 
 # for i in markets.targetList:
 #     coin = f'{i}'
@@ -14,10 +14,10 @@ import markets
 # print('Market :', coin)
 
 while True:
+    import markets
     for i in markets.targetList:
         coin = i
-        print('''-----------------------------------------------
-        Market :''', coin)
+
         # BALANCE
         c = ccxt.ftx({
             'apiKey': 'VYXBrkmuhutN9cr2APKbblqW4esKX-0Euhe9evr4',
@@ -30,7 +30,11 @@ while True:
         balance = float(balance['total']['USD'])
         position = pd.DataFrame(c.fetch_positions())
         position = position.set_index('future')
-        BTC = float(position[position.index == f"{coin}"]['size'])
+        BTC = position[position.index == f"{coin}"]['size'].apply(pd.to_numeric, errors='ignore')
+        wallet = pd.DataFrame(c.fetch_account_positions()).apply(pd.to_numeric, errors='ignore')
+        wallet = wallet[wallet['size'] > 0]
+        print(wallet)
+
 
 
         # STRATEGY
@@ -39,11 +43,17 @@ while True:
         historical = requests.get(
             'https://ftx.com/api/markets/{}/candles?resolution=900&start_time=1609462800'.format(coin)).json()
         historical = pd.DataFrame(historical['result'])
+
+        recent = requests.get(
+            'https://ftx.com/api/markets/{}/candles?resolution=15&start_time=1609462800'.format(coin)).json()
+        recent = pd.DataFrame(recent['result'])
+        recent = recent.loc[:, ['open', 'close', 'high', 'low']].iloc[-1]['open']
         ma_20 = historical['close'].tail(20).mean()
         historical = historical.loc[:, ['open', 'close', 'high', 'low']]
 
         X = 0.5
-
+        print('''-----------------------------------------------
+        Market :''', coin)
         print(historical.tail(3))
         print('잔액 =', balance, 'USD')
         print('미체결:', BTC)
@@ -56,20 +66,20 @@ while True:
         try:
             btc_data = requests.get(f'https://ftx.com/api/markets/{coin}').json()
             print(f"레버리지 ={BTC/(balance / btc_data['result']['ask']):.2f} 배")
-            print(f"현재가 = {btc_data['result']['ask']:.8f}")
+            print(f"현재가 = {recent:.8f}")
             T1 = historical.iloc[-1]['open'] + (historical.iloc[-2]['high'] - historical.iloc[-2]['low']) * X
             print('매수기준 =', T1, '20ma=',ma_20)
         except Exception as e:
             print(f'Error obtaining {coin} old data: {e}')
         #매수조건
-        if btc_data['result']['ask'] >= T1 >ma_20:
+        if recent >= T1 >ma_20:
             if float(datetime.datetime.now().strftime('%S')) > 30:
                 T1 = historical.iloc[-1]['open'] + (historical.iloc[-2]['high'] - historical.iloc[-2]['low']) * X
                 # print('매수기준 =', T1)
                 # T2 = historical.iloc[-1]['open'] - (historical.iloc[-2]['high'] - historical.iloc[-2]['low']) * X
                 # print('매도기준 =', T2)
-                if BTC*btc_data['result']['ask'] < 0.5 * balance:
-                    r = c.place_order(f'{coin}', "buy", btc_data['result']['ask'], 0.2*balance/btc_data['result']['ask'],client_id =datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                if BTC*recent < 0.2 * balance:
+                    r = c.place_order(f'{coin}', "buy", 1.01*recent, 0.2*balance/btc_data['result']['ask'],client_id =datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
                     print(r)
                 else: print('balance was not satisfied.')
                 sleep(2)
@@ -77,16 +87,16 @@ while True:
                 print('time was not satisfied.')
             sleep(2)
         #매도조건
-        elif BTC != 0:
-        # if btc_data['result']['ask'] < T2:
-            if float(datetime.datetime.now().strftime('%M')) % 15 == 0:
-                if float(datetime.datetime.now().strftime('%S')) < 30:
-                    # T1 = historical.iloc[-1]['open'] + (historical.iloc[-2]['high'] - historical.iloc[-2]['low']) * X
-                    # print('매수기준 =', T1)
-                    # T2 = historical.iloc[-1]['open'] - (historical.iloc[-2]['high'] - historical.iloc[-2]['low']) * X
-                    # print('매도기준 =', T2)
-                    s = c.place_order(f'{coin}', "sell", btc_data['result']['bid'], BTC, reduce_only=True,
+
+        if float(datetime.datetime.now().strftime('%M')) % 15 == 0:
+            if float(datetime.datetime.now().strftime('%S')) < 30:
+                # T1 = historical.iloc[-1]['open'] + (historical.iloc[-2]['high'] - historical.iloc[-2]['low']) * X
+                # print('매수기준 =', T1)
+                # T2 = historical.iloc[-1]['open'] - (historical.iloc[-2]['high'] - historical.iloc[-2]['low']) * X
+                # print('매도기준 =', T2)
+                for w in wallet['future']:
+                    s = c.place_order(f'{w}', "sell", 0.99*recent, wallet[wallet['future']==w]['size'], reduce_only=True,
                                       client_id=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-                    print(s)
-                    sleep(2)
+                print(s)
+                sleep(2)
         else: print('The buy requirement was not satisfied.')
